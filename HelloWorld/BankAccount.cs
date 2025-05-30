8f0bc916a30d8bdf8b625839d74c6f2471ddd2ca
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2025-05-30 15:56:40 +0530

    encapsulation explained

---
+++ HelloWorld/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class BankAccount
+    {
+        private decimal balance;
+
+
+        public BankAccount(decimal balance)
+        {
+            Deposit(balance);
+        }
+
+        public decimal GetBalance()
+        {
+
+            return balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if(amount<=0)
+            {
+                Console.WriteLine("the amount cannot be negative");
+            }
+               
+
+            balance += amount;
+        }
+
+
+        public void withdraw(decimal amount) {
+
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount must be greater than 0");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("withdrawl amount cannot be more than balance amount")
+            }
+
+            balance += amount;
+
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,8 +2,18 @@
 
 using HelloWorld;
 
-BadBankAccount badBankAccount=new BadBankAccount();
+//BadBankAccount badBankAccount=new BadBankAccount();
 
-badBankAccount.balance = -50;
+//badBankAccount.balance = -50;
 
-Console.WriteLine(badBankAccount.balance);
+//Console.WriteLine(badBankAccount.balance);
+
+
+
+BankAccount bankacount=new BankAccount(200);
+
+
+Console.WriteLine(bankacount.GetBalance());
+bankacount.Deposit(100);
+
+Console.WriteLine(bankacount.GetBalance());
